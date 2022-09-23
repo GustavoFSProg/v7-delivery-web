9d890cacda63b8c63cb7cbd8ac66a75e781924a6
Gustavo <gustavo.prog40@gmail.com>
2022-09-23 09:52:17 -0300

    first

---
+++ pages/[tenant]/index.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem-Vindo (a) 👋</div>
+
+            <div className={styles.headerSubTitle}>Oque Deseja pra hoje?</div>
+          </div>
+
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButtom}>
+              <div className={styles.menuButtomLines}>
+
+            </div>
+              <div className={styles.menuButtomLines}>
+
+              </div>
+              <div className={styles.menuButtomLines}>
+
+            </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerBottom}>Busca</div>
+      </header>
+    </div>
+  )
+}
+
+export default Home
