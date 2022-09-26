02f29c2ce580738dd5d137965db52b58fca9a0e4
Gustavo <gustavo.prog40@gmail.com>
2022-09-26 17:52:59 -0300

    banners

---
+++ components/banner/index.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.css'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import banner01 from '../../assets/banner-01.png'
+import banner02 from '../../assets/banner-02.png'
+
+// Import Swiper styles
+import 'swiper/css'
+import Image from 'next/image'
+
+function Banner() {
+  return (
+    <div className={styles.container}>
+      <Swiper slidesPerView={1}
+      >
+        <SwiperSlide>
+          <Image src={banner01} alt="banner 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={banner02} alt="banner 2" />
+        </SwiperSlide>
+        
+      </Swiper>
+    </div>
+  )
+}
+
+export default Banner
