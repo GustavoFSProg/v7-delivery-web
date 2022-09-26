4a81d1a7baaed46820c4aac139e963f4f5cbe9ac
Gustavo <gustavo.prog40@gmail.com>
2022-09-26 09:43:52 -0300

    line two

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -6,6 +6,8 @@ import styles from '../../styles/Home.module.css'
 import burguerImage from '../../assets/Card-burger.png'
 import TexasBurguer from '../../assets/TexasBurger.png'
 import GoldenBurguer from '../../assets/GoldenBurger.png'
+import MonsterBurguer from '../../assets/MonsterBurger.png'
+import OldBurguer from '../../assets/OldBurger.png'
 
 const Home: NextPage = () => {
   return (
@@ -82,33 +84,42 @@ const Home: NextPage = () => {
         <div className={styles.containerFotos}>
           <div className={styles.containerLeft}>
             <Image className={styles.TexasBurguer} src={TexasBurguer} alt="burguer image" />
-            <span className={styles.paragraphTradicional}>
-              Tradicional
-            </span>
+            <span className={styles.paragraphTradicional}>Tradicional</span>
 
-             <span className={styles.texasBurguer}>
-              Texas Burguer
-            </span>
+            <span className={styles.texasBurguer}>Texas Burguer</span>
 
-              <span className={styles.burguerPrice}>
-              R$ 30,00
-            </span>
+            <span className={styles.burguerPrice}>R$ 25,00</span>
           </div>
 
           <div className={styles.containerRight}>
             <Image className={styles.GoldenBurguer} src={GoldenBurguer} alt="burguer image" />
 
-              <span className={styles.paragraphTradicionalDois}>
-              Tradicional
-            </span>
+            <span className={styles.paragraphTradicionalDois}>Tradicional</span>
 
-             <span className={styles.goldenBurguerTitle}>
-              Golden Burguer
-            </span>
+            <span className={styles.goldenBurguerTitle}>Golden Burguer</span>
 
-              <span className={styles.burguerPrice}>
-              R$ 30,00
-            </span>
+            <span className={styles.burguerPrice}>R$ 28,00</span>
+          </div>
+        </div>
+
+        <div className={styles.containerFotos} style={{marginTop: '20px'}}>
+          <div className={styles.containerLeft}>
+            <Image className={styles.TexasBurguer} src={MonsterBurguer} alt="burguer image" />
+            <span className={styles.paragraphTradicional}>Tradicional</span>
+
+            <span className={styles.texasBurguer}>Texas Burguer</span>
+
+            <span className={styles.burguerPrice}>R$ 25,00</span>
+          </div>
+
+          <div className={styles.containerRight}>
+            <Image className={styles.GoldenBurguer} src={OldBurguer} alt="burguer image" />
+
+            <span className={styles.paragraphTradicionalDois}>Tradicional</span>
+
+            <span className={styles.goldenBurguerTitle}>Golden Burguer</span>
+
+            <span className={styles.burguerPrice}>R$ 27,00</span>
           </div>
         </div>
       </header>
